eb7018ec307c6bdfb7fc5b28e85d9c6f69ef5619
hasano14 <bang_apeez@yahoo.com>
2022-07-14 16:52:32 +0800

    Changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { AiOutlineMenu } from "react-icons/ai";
+import { FiShoppingCart } from "react-icons/fi";
+import { BsChatLeft } from "react-icons/bs";
+import { RiNotification3Line } from "react-icons/ri";
+import { MdKeyboardArrowDown } from "react-icons/md";
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
+
+import avatar from "../data/avatar.jpg";
+import { Cart, Chat, Notification, userProfile } from ".";
+import { useStateContext } from "../contexts/ContextProvider";
+
+const NavButton = (title, customFunc, icon, color, dotColor) => (
+  <TooltipComponent content={title} position="BottomCenter">
+    <button
+      type="button"
+      onClick={customFunc}
+      style={{ color }}
+      className=""
+    ></button>
+  </TooltipComponent>
+);
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const { activeMenu, setActiveMenu } = useStateContext();
 
-export default Navbar
+  return (
+    <div className="flex justify-between p-2 md:mx-6 relative">
+      <NavButton
+        title="Menu"
+        customFunc={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
+    </div>
+  );
+};
+
+export default Navbar;
