2ba8ddbd7b4bef99101357618f403c794918978b
hasano14 <bang_apeez@yahoo.com>
2022-07-15 21:51:39 +0800

    Changes

--- client/src/contexts/ContextProvider.js
+++ client/src/contexts/ContextProvider.js
@@ -13,11 +13,21 @@ export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
   const handleClick = (clicked) => {
-    setIsClicked({...initialState, [clicked] : true})
-  }
-
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
+  const [screenSize, setScreenSize] = useState(undefined);
   return (
-    <StateContext.Provider value={{ activeMenu, setActiveMenu,isClicked, setIsClicked }}>
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
+        isClicked,
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize,
+      }}
+    >
       {children}
     </StateContext.Provider>
   );
